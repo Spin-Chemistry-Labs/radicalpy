6803ac395aad622b297b1083a8c7d41944ee5e9f
Emil VATAI <emil.vatai@gmail.com>
2022-05-06 18:09:29 +0900

    Update version and URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="radicalpy",
-    version="0.1a",
+    version="0.1b",
     license="MIT",
     author="Lewis M. Antill",
     author_email="lewismantill@gmail.com",
@@ -12,7 +12,7 @@ setup(
     maintainer_email="emil.vatai@gmail.com",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    url="https://github.com/lmantill/radicalpy",
+    url="https://github.com/Spin-Chemistry-Labs/radicalpy",
     keywords="quantum spin chemistry",
     install_requires=[
         "numpy",
