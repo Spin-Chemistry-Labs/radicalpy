8597422b641a49e2d85d5fc7e44203139b7e968e
Emil VATAI <emil.vatai@gmail.com>
2022-06-20 22:52:02 +0900

    Start development

---
+++ src/radicalpy/simulation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+# This is just something based on some earlier scripts... nothing is
+# set in stone yet.
+
+
+class Sim:
+    def __init__(self, n):
+        self.data = np.ones(n)
--- tests/test_radicalpy.py
+++ tests/test_radicalpy.py
@@ -1,6 +1,13 @@
 import unittest
 
+import numpy as np
+from src.radicalpy import Sim
+
 
 class DummyTests(unittest.TestCase):
+    def setUp(self):
+        self.n = 10
+        self.sim = Sim(self.n)
+
     def test_succeed(self):
-        pass
+        assert np.all(self.sim.data == np.ones(self.n))
