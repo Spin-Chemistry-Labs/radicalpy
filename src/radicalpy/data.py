d06f166f1ec566ba2c5371c9ca30f748b3d4d825
Emil VATAI <emil.vatai@gmail.com>
2022-07-29 10:43:35 +0900

    Read molecules

--- src/radicalpy/data.py
+++ src/radicalpy/data.py
@@ -1,6 +1,18 @@
 import json
 from pathlib import Path
 
-datadir = Path(__file__).parent / "data"
+DATA_DIR = Path(__file__).parent / "data"
+SPIN_DATA_JSON = "spin_data.json"
+MOLECULES_DIR = DATA_DIR / "molecules"
 
-spin_data = json.load(open(datadir / "spin_data.json"))
+
+def get_molecules(molecules_dir=MOLECULES_DIR):
+    molecules = {}
+    for json_path in molecules_dir.glob("*.json"):
+        molecule_name = json_path.with_suffix("").name
+        molecules[molecule_name] = json.load(open(json_path))
+    return molecules
+
+
+spin = json.load(open(DATA_DIR / SPIN_DATA_JSON))
+molecules = get_molecules()
