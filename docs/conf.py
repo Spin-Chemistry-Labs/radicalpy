14940ed107a6e50ace7b2d5c3bac1b2a5c505e9d
Emil VATAI <emil.vatai@gmail.com>
2022-08-03 23:02:52 +0900

    Update and clean up stuff

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
@@ -37,5 +38,6 @@ todo_include_todos = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+# html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
