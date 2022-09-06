2561221018164beaf1f4407276b3bc74ff58f747
Emil VATAI <emil.vatai@gmail.com>
2022-09-06 12:00:52 +0900

    Refactor `probability_from_density()`

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -183,17 +183,18 @@ class SimulationTests(unittest.TestCase):
         H = self.sim.total_hamiltonian(B, J, D)
         for init_state in self.states:
             for obs_state in self.states:
-                evol = self.sim.hilbert_time_evolution(init_state, obs_state, time, H)
+                rhos = self.sim.hilbert_time_evolution(init_state, time, H)[1:]
                 evol_true = radpy.TimeEvolution(
                     self.spins, init_state, obs_state, t_max, dt, k, 0, H, "Hilbert"
                 )
-                assert np.all(
-                    np.isclose(evol["evol"][1:], evol_true[1][:-1])
-                ), "Time evolution failed"
-                assert np.all(
-                    np.isclose(evol["rho"][1:], evol_true[-1][:-1])
-                ), "Time evolution (rho) failed)"
+                prob_true = evol_true[1][:-1]
+                rhos_true = evol_true[-1][:-1]
+                prob = self.sim.probability_from_density(obs_state, rhos)
 
+                chk_prob = np.isclose(prob, prob_true)
+                assert np.all(chk_prob), "Time evolution (probability) failed)"
+                chk_rhos = np.isclose(rhos, rhos_true)
+                assert np.all(chk_rhos), "Time evolution (rho) failed)"
 
     @unittest.skip("Keeping only for the notes from earlier")
     def test_dummy(self):
