a3349de6f2ae93866f767503109005228de1a279
Emil VATAI <emil.vatai@gmail.com>
2022-09-20 23:03:28 +0900

    Figure out `doctest`s

--- src/radicalpy/data/__init__.py
+++ src/radicalpy/data/__init__.py
@@ -5,21 +5,23 @@ DATA_DIR = Path(__file__).parent
 SPIN_DATA_JSON = DATA_DIR / "spin_data.json"
 MOLECULES_DIR = DATA_DIR / "molecules"
 
-SPIN_DATA = json.load(open(SPIN_DATA_JSON))
-"""Dictionary containing spin data for elements.
+with open(SPIN_DATA_JSON) as f:
+    SPIN_DATA = json.load(f)
+    """Dictionary containing spin data for elements.
 
-:meta hide-value:"""
+    :meta hide-value:"""
 
 
-def _get_molecules(molecules_dir=MOLECULES_DIR):
+def get_molecules(molecules_dir=MOLECULES_DIR):
     molecules = {}
     for json_path in molecules_dir.glob("*.json"):
         molecule_name = json_path.with_suffix("").name
-        molecules[molecule_name] = json.load(open(json_path))
+        with open(json_path) as f:
+            molecules[molecule_name] = json.load(f)
     return molecules
 
 
-MOLECULE_DATA = _get_molecules()
+MOLECULE_DATA = get_molecules()
 """Dictionary containing data for each molecule.
 
 :meta hide-value: """
