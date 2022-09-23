c5ad48e477eb18573080ef1f5c25809992779268
Emil VATAI <emil.vatai@gmail.com>
2022-09-23 23:29:29 +0900

    WIP Kinetics (need sim object)

--- src/radicalpy/kinetics.py
+++ src/radicalpy/kinetics.py
@@ -30,3 +30,33 @@ class KineticsDiffusion(KineticsRelaxationBase):
         A = numerator / denominator
         B = ((self.r0 - self.r_sigma) ** 2) / (4 * self.diffusion_coefficient)
         product_probabilities *= A * time ** (-3 / 2) * np.exp(-B / time)
+
+
+# class KineticsBase(KineticsRelaxationBase):
+#     def convert(Q):
+#         return np.kron(Q, np.eye(len(Q))) + (np.kron(np.eye(len(Q)), Q))
+
+
+# class KineticsHaberkornSinglet(KineticsBase):
+#     def adjust_hamiltonian(self, H):
+
+#         H -= 0.5 * self.k * self.convert(projop(spins, "S"))
+
+
+# class KineticsHaberkornTriplet(KineticsBase):
+#     def adjust_hamiltonian(self, H):
+#         H -= 0.5 * self.k * self.convert(projop(spins, "T"))
+
+
+# class KineticsHaberkornFreeRadical(KineticsBase):
+#     def adjust_hamiltonian(self, H):
+#         H -= self.k * np.eye(*H.shape)
+
+
+# class KineticsJonesHore(KineticsBase):
+#     def adjust_hamiltonian(self, H):
+#         H -= (
+#             0.5 * ks * self.convert(QS)
+#             + 0.5 * kt * self.convert(QT)
+#             + (0.5 * (ks + kt)) * (np.kron(QS, QT) + np.kron(QT, QS))
+#         )
