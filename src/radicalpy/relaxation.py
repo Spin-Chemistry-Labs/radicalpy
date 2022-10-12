7de3cef827697426370e0754802e8787d8b7afc6
Emil VATAI <emil.vatai@gmail.com>
2022-10-12 12:26:52 +0900

    WIP Relaxations

--- src/radicalpy/relaxation.py
+++ src/radicalpy/relaxation.py
@@ -1,84 +1,92 @@
 import numpy as np
 
-from .simulation import KineticsRelaxationBase
+from .simulation import KineticsRelaxationBase, LiouvilleSimulation, State
+
+
+class RelaxationBaseST(KineticsRelaxationBase):
+    @staticmethod
+    def __init__(self, rate_constant: float, sim: LiouvilleSimulation):
+        super().__init__(rate_constant)
+        self.QS = sim.projection_operator(State.SINGLET)
+        self.QT = sim.projection_operator(State.TRIPLET)
+
+
+class RelaxationBaseAll(RelaxationBaseST):
+    @staticmethod
+    def __init__(self, rate_constant: float, sim: LiouvilleSimulation):
+        super().__init__(rate_constant, sim)
+        self.QTp = sim.projection_operator(State.TRIPLET_PLUS)
+        self.QTm = sim.projection_operator(State.TRIPLET_MINUS)
+        self.QT0 = sim.projection_operator(State.TRIPLET_ZERO)
 
 
 def fun():
     SAx, SAy, SAz = spinops(0, spins)
     SBx, SBy, SBz = spinops(1, spins)
 
-    QS = projop(spins, "S")
-    QT = projop(spins, "T")
-    QTp = projop(spins, "Tp")
-    QTm = projop(spins, "Tm")
-    QT0 = projop(spins, "T0")
+
+class STD(RelaxationBaseAll):
+    def adjust_hamiltonian(self, H: np.ndarray):
+        H -= self.k * (np.kron(self.QS, self.QT) + np.kron(self.QT, self.QS))
 
 
-def STD():
-    return k * (np.kron(QS, QT) + np.kron(QT, QS))
-
-
-def TTD():
-    return k * (
-        np.kron(QTp, QTm)
-        + np.kron(QTm, QTp)
-        + np.kron(QT0, QTm)
-        + np.kron(QTm, QT0)
-        + np.kron(QTp, QT0)
-        + np.kron(QT0, QTp)
-    )
-
-
-def TTR():
-    return k * (
-        (
-            2 / 3 * (np.kron(QT0, QT0))
-            + (
-                1
-                / 3
-                * (
-                    (
-                        np.kron(QTp, QTp)
-                        + np.kron(QTm, QTm)
-                        + np.kron(QTp, QTm)
-                        + np.kron(QTm, QTp)
-                    )
-                    - (
-                        np.kron(QTp, QT0)
-                        - np.kron(QT0, QTp)
-                        - np.kron(QTm, QT0)
-                        - np.kron(QT0, QTm)
-                        - np.kron(QTp, QTm)
-                        - np.kron(QTm, QTp)
-                    )
-                )
-            )
+class TTF(RelaxationBaseAll):
+    def adjust_hamiltonian(self, H: np.ndarray):
+        H -= self.k * (
+            np.kron(self.QTp, self.QTm)
+            + np.kron(self.QTm, self.QTp)
+            + np.kron(self.QT0, self.QTm)
+            + np.kron(self.QTm, self.QT0)
+            + np.kron(self.QTp, self.QT0)
+            + np.kron(self.QT0, self.QTp)
         )
-    )
 
 
-def RFR():
-    return k * (
-        1.5 * np.kron(np.eye(len(QS)), np.eye(len(QS)))
-        - np.kron(SAx, SAx.T)
-        - np.kron(SAy, SAy.T)
-        - np.kron(SAz, SAz.T)
-        - np.kron(SBx, SBx.T)
-        - np.kron(SBy, SBy.T)
-        - np.kron(SBz, SBz.T)
-    )
+class TTR(RelaxationBaseAll):
+    def adjust_hamiltonian(self, H: np.ndarray):
+        temr0 = np.kron(self.QT0, self.QT0)
+        term1 = (
+            np.kron(self.QTp, self.QTp)
+            + np.kron(self.QTm, self.QTm)
+            + np.kron(self.QTp, self.QTm)
+            + np.kron(self.QTm, self.QTp)
+        )
+        temr2 = (
+            np.kron(self.QTp, self.QT0)
+            - np.kron(self.QT0, self.QTp)
+            - np.kron(self.QTm, self.QT0)
+            - np.kron(self.QT0, self.QTm)
+            - np.kron(self.QTp, self.QTm)
+            - np.kron(self.QTm, self.QTp)
+        )
+        H -= self.k * (2 / 3 * temr0 + 1 / 3 * (term1 - temr2))
 
 
-def DM():
-    return k * (
-        1 / 9 * np.kron(QS, QTp)
-        + 1 / 9 * np.kron(QTp, QS)
-        + 1 / 9 * np.kron(QS, QTm)
-        + 1 / 9 * np.kron(QTm, QS)
-        + 4 / 9 * np.kron(QS, QT0)
-        + 4 / 9 * np.kron(QT0, QS)
-        + np.kron(QTp, QT0)
-        + np.kron(QT0, QTp)
-        + np.kron(QTm, QT0)
-        + np.kron(QT0, QTm)
-    )
+class RFR(RelaxationBaseAll):
+    ################ TODO SAx
+    def adjust_hamiltonian(self, H: np.ndarray):
+        H -= self.k * (
+            1.5 * np.kron(np.eye(len(self.QS)), np.eye(len(self.QS)))
+            - np.kron(self.SAx, self.SAx.T)
+            - np.kron(self.SAy, self.SAy.T)
+            - np.kron(self.SAz, self.SAz.T)
+            - np.kron(self.SBx, self.SBx.T)
+            - np.kron(self.SBy, self.SBy.T)
+            - np.kron(self.SBz, self.SBz.T)
+        )
+
+
+class DM(RelaxationBaseAll):
+    def adjust_hamiltonian(self, H: np.ndarray):
+        H -= self.k * (
+            1 / 9 * np.kron(self.QS, self.QTp)
+            + 1 / 9 * np.kron(self.QTp, self.QS)
+            + 1 / 9 * np.kron(self.QS, self.QTm)
+            + 1 / 9 * np.kron(self.QTm, self.QS)
+            + 4 / 9 * np.kron(self.QS, self.QT0)
+            + 4 / 9 * np.kron(self.QT0, self.QS)
+            + np.kron(self.QTp, self.QT0)
+            + np.kron(self.QT0, self.QTp)
+            + np.kron(self.QTm, self.QT0)
+            + np.kron(self.QT0, self.QTm)
+        )
