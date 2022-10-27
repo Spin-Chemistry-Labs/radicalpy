28fb5ed7748a5d8cf74672074706274201eaca4b
lmantill <72053628+lmantill@users.noreply.github.com>
2022-10-27 21:52:38 +0900

    Add mT_to_angular_frequency conversion

--- src/radicalpy/utils.py
+++ src/radicalpy/utils.py
@@ -24,3 +24,10 @@ def mT_to_MHz(mT: float) -> float:
     mu_B = constants.value("mu_B")
     h = constants.value("h")
     return mT * 1e-9 * g_e * mu_B / h
+
+
+def mT_to_angular_frequency(mT: float) -> float:
+    g_e = constants.value("g_e")
+    mu_B = constants.value("mu_B")
+    hbar = constants.value("hbar")
+    return mT * mu_B / hbar * g_e / 1e9
