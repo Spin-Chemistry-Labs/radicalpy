0414ca8786c27073042fe636861f46d18a4f8675
lmantill <72053628+lmantill@users.noreply.github.com>
2022-10-29 15:29:00 +0900

    Add rotational correlation time

--- src/radicalpy/estimations.py
+++ src/radicalpy/estimations.py
@@ -6,17 +6,26 @@ from . import utils
 from .data import constants
 
 
+def rotational_correlation_time(radius, temp, eta=0.89e-3):
+    k_B = constants.value("k_B")
+
+    # Calculate isotropic rotational correlation time (tau_c) in s
+    tau_c = (4 * np.pi * eta * radius**3) / (3 * k_B * temp)
+    return tau_c
+
+
 def rotational_correlation_time_protein(Mr, temp, eta=0.89e-3):
     V = constants.value("V")
     rw = constants.value("rw")
     N_A = constants.value("N_A")
-    k_B = constants.value("k_B")
+    #k_B = constants.value("k_B")
 
     # Calculate Rh - effective hydrodynamic radius of the protein in m
     Rh = ((3 * V * Mr) / (4 * np.pi * N_A)) ** 0.33 + rw
 
     # Calculate isotropic rotational correlation time (tau_c) in s
-    tau_c = (4 * np.pi * eta * Rh**3) / (3 * k_B * temp)
+	tau_c = rotational_correlation_time(Rh, temp, eta)
+    #tau_c = (4 * np.pi * eta * Rh**3) / (3 * k_B * temp)
     return tau_c
 
 
