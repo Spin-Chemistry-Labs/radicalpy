e15777ff497bcfa283fd679ef8103b245d077898
Emil VATAI <emil.vatai@gmail.com>
2022-10-31 17:12:34 +0900

    Bump up version (0.4rc1)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="radicalpy",
-    version="0.3rc2",
+    version="0.4rc1",
     license="MIT",
     author="Lewis M. Antill",
     author_email="lewismantill@gmail.com",
