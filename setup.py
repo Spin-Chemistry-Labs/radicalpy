a1592af6b8a3c61bf7a0b5b56a85be0d800c0e7d
LewisAntill <72053628+lmantill@users.noreply.github.com>
2022-10-31 18:02:28 +0900

    Bump up version (0.4rc3)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="radicalpy",
-    version="0.4rc2",
+    version="0.4rc3",
     license="MIT",
     author="Lewis M. Antill",
     author_email="lewismantill@gmail.com",
