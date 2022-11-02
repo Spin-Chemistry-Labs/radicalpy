43c5af4b01987df95e16f7cf62b35f6caafa94a1
Emil VATAI <emil.vatai@gmail.com>
2022-11-02 16:39:32 +0900

    WIP

--- src/radicalpy/hfc3d.py
+++ src/radicalpy/hfc3d.py
@@ -1,3 +1,4 @@
+import json
 from collections import OrderedDict
 
 import numpy as np
@@ -13,7 +14,6 @@ def isotropic(anisotropic: np.ndarray):
 
 
 def get_srp():
-
     flavin_data = data.MOLECULE_DATA["flavin_anion"]["data"]
     rp = np.zeros(N)
     rp_keys = []
@@ -46,11 +46,40 @@ if __name__ == "__main__":
 
     srp = get_srp()
     sor = get_sor()
+    dsor = OrderedDict(sor)
     new = OrderedDict()
+
+    H_21_22_23 = (dsor["18H"] + dsor["19H"] + dsor["20H"]) / 3
+    new["H21"] = H_21_22_23
+    new["H22"] = H_21_22_23
+    new["H23"] = H_21_22_23
+    new["H24"] = dsor["24H"]
+    new["N16"] = dsor["15N"]
+    new["H31"] = dsor["26H"]
+    new["N14"] = dsor["2N"]
+    new["H28"] = dsor["28H"]
+    new["H20"] = dsor["25H"]
+    H_25_26_27 = (dsor["21H"] + dsor["22H"] + dsor["23H"]) / 3
+    new["H25"] = H_25_26_27
+    new["H26"] = H_25_26_27
+    new["H27"] = H_25_26_27
+    new["N6"] = dsor["4N"]
+    new["H29"] = dsor["29H"]
+    new["H30"] = dsor["30H"]
+    new["N5"] = dsor["11N"]
+    nlst = list(new.items())
+    nlst = sorted(nlst, key=lambda t: isotropic(t[1]))
 
     for i in range(N):
         print(
-            f"{i=:2} {srp[i][1]:7} {srp[i][0]:5} {isotropic(sor[i][1]):10.5} {sor[i][0]:5} {i=:2}"
+            f"{i=:2} {srp[i][1]:7} {srp[i][0]:5} {nlst[i][0]:5} {isotropic(nlst[i][1]):10.5} {isotropic(sor[i][1]):10.5} {sor[i][0]:5} {i=:2}"
         )
-    # dsor = OrderedDict(sor)
-    # print(list(dsor.items()))
+
+    flavin_data = data.MOLECULE_DATA["flavin_anion"]
+    new_flavin = dict(flavin_data)
+    for k, v in new.items():
+        new_flavin["data"][k]["hfc"] = list(map(lambda t: list(t), v))
+
+    # print(new_flavin)
+    with open("flavin_anion.json", "w") as f:
+        json.dump(new_flavin, f, indent=2)
