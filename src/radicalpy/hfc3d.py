64349c2cdc5c82b49f5ecab5e490657a597d1b55
Emil VATAI <emil.vatai@gmail.com>
2022-11-02 19:14:30 +0900

    Flavin processing good

--- src/radicalpy/hfc3d.py
+++ src/radicalpy/hfc3d.py
@@ -8,35 +8,36 @@ import utils
 from . import data, utils
 from .flavin_3x3 import flavin as molecule
 
-MOLECULE = "flavin_anion"
-# from .TRP_3x3 import TRP as molecule
-# MOLECULE = "tryptophan_cation"
 # from .TYRrad import TYRrad as molecule
-
-# MOLECULE = "tyrosine_neutral"
+# from .trp import TRP as molecule
 
 
 def isotropic(anisotropic: np.ndarray):
     return anisotropic.trace() / 3
 
 
+MOLECULE = "flavin_anion"
+# MOLECULE = "tryptophan_cation"
+# MOLECULE = "tyrosine_neutral"
+
+
 def get_srp():
-    flavin_data = data.MOLECULE_DATA[MOLECULE]["data"]
-    rp = np.zeros(N)
+    molecule_data = data.MOLECULE_DATA[MOLECULE]["data"]
+    rp = []
     rp_keys = []
-    for i, (k, v) in enumerate(flavin_data.items()):
-        rp[i] = v["hfc"]
+    for k, v in molecule_data.items():
+        rp.append(v["hfc"])
         rp_keys.append(k)
 
     zrp = zip(rp_keys, rp)
-    srp = sorted(zrp, key=lambda t: t[1])
+    srp = sorted(zrp, key=lambda t: t[1], reverse=True)
     return srp
 
 
 def get_sor():
     orca = []
     orca_keys = []
-    for i, (k, v) in enumerate(molecule.items()):
+    for k, v in molecule.items():
         m = np.array(v)
         # m = isotropic(m)
         m = utils.MHz_to_mT(m)
@@ -44,7 +45,7 @@ def get_sor():
         orca_keys.append(k)
 
     zor = zip(orca_keys, orca)
-    sor = sorted(zor, key=lambda t: isotropic(t[1]))
+    sor = sorted(zor, key=lambda t: isotropic(t[1]), reverse=True)
     return sor
 
 
@@ -56,39 +57,41 @@ if __name__ == "__main__":
     dsor = OrderedDict(sor)
     new = OrderedDict()
     print(len(srp), len(sor))
-    assert len(srp) == N
 
-    # H_21_22_23 = (dsor["18H"] + dsor["19H"] + dsor["20H"]) / 3
-    # new["H21"] = H_21_22_23
-    # new["H22"] = H_21_22_23
-    # new["H23"] = H_21_22_23
-    # new["H24"] = dsor["24H"]
-    # new["N16"] = dsor["15N"]
-    # new["H31"] = dsor["26H"]
-    # new["N14"] = dsor["2N"]
-    # new["H28"] = dsor["28H"]
-    # new["H20"] = dsor["25H"]
-    # H_25_26_27 = (dsor["21H"] + dsor["22H"] + dsor["23H"]) / 3
-    # new["H25"] = H_25_26_27
-    # new["H26"] = H_25_26_27
-    # new["H27"] = H_25_26_27
-    # new["N6"] = dsor["4N"]
-    # new["H29"] = dsor["29H"]
-    # new["H30"] = dsor["30H"]
-    # new["N5"] = dsor["11N"]
-    # nlst = list(new.items())
-    # nlst = sorted(nlst, key=lambda t: isotropic(t[1]))
-    nlst = list(sor)
+    H_21_22_23 = (dsor["18H"] + dsor["19H"] + dsor["20H"]) / 3
+    new["H21"] = H_21_22_23
+    new["H22"] = H_21_22_23
+    new["H23"] = H_21_22_23
+    new["H24"] = dsor["24H"]
+    new["N16"] = dsor["15N"]
+    new["H31"] = dsor["26H"]
+    new["N14"] = dsor["2N"]
+    new["H28"] = dsor["28H"]
+    new["H20"] = dsor["25H"]
+    H_25_26_27 = (dsor["21H"] + dsor["22H"] + dsor["23H"]) / 3
+    new["H25"] = H_25_26_27
+    new["H26"] = H_25_26_27
+    new["H27"] = H_25_26_27
+    new["N6"] = dsor["4N"]
+    new["H29"] = dsor["29H"]
+    new["H30"] = dsor["30H"]
+    new["N5"] = dsor["11N"]
+    nlst = list(new.items())
+    nlst = sorted(nlst, key=lambda t: isotropic(t[1]), reverse=True)
+    # nlst = list(sor)
 
+    print(f"{MOLECULE}")
     print("idx  json (old)    json(new)          orca")
     for i in range(N):
         print(
-            f"{i=:2} {srp[i][1]:7} {srp[i][0]:5} {nlst[i][0]:5} {isotropic(nlst[i][1]):10.5} {isotropic(sor[i][1]):10.5} {sor[i][0]:5} {i=:2}"
+            f"{i=:2} {srp[i][1]:7} {srp[i][0]:5} {nlst[i][0]:6} {isotropic(nlst[i][1]):10.5} {isotropic(sor[i][1]):10.5} {sor[i][0]:8} {i=:2}"
         )
 
     new_molecule = dict(data.MOLECULE_DATA[MOLECULE])
     for k, v in new.items():
         new_molecule["data"][k]["hfc"] = list(map(lambda t: list(t), v))
+
+    new_molecule["data"]["N10"] = new_molecule["data"].pop("N6")
 
     with open(f"{MOLECULE}.json", "w") as f:
         json.dump(new_molecule, f, indent=2)
