4994843cde474e4bd82c06885832dd58b4b6cfcd
Emil VATAI <emil.vatai@gmail.com>
2022-11-22 11:42:15 +0900

    Add missing Haberkorn

--- examples/density_matrix_gif.py
+++ examples/density_matrix_gif.py
@@ -21,7 +21,10 @@ def main():
         D=0,
         J=0,
         # kinetics=[rp.kinetics.Exponential(k)],
-        kinetics=[rp.kinetics.Haberkorn(k, State.SINGLET)],
+        kinetics=[
+            rp.kinetics.Haberkorn(k, State.SINGLET),
+            rp.kinetics.Haberkorn(k, State.TRIPLET),
+        ],
     )
 
     bar3d_kwargs = {"alpha": 0.9}
