6f2adbe8fc4b9168c7b9c71f6c678a0facfbd1ae
lmantill <72053628+lmantill@users.noreply.github.com>
2022-11-23 17:31:06 +0900

    made mary example

--- examples/mary.py
+++ examples/mary.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import radicalpy as rp
 from radicalpy.simulation import State
+from radicalpy import relaxation
 
 
 def main():
     flavin = rp.simulation.Molecule("flavin_anion", ["H25"])
     Z = rp.simulation.Molecule("Z")
-    sim = rp.simulation.HilbertSimulation([flavin, Z])
-    time = np.arange(0, 15-6, 5e-9)
-    Bs = np.arange(0, 10, 1)
+    sim = rp.simulation.LiouvilleSimulation([flavin, Z])
+    time = np.arange(0, 15e-6, 5e-9)
+    Bs = np.arange(0, 20, 0.1)
     k = 3e6
 
     MARY = sim.MARY(
@@ -22,16 +23,18 @@ def main():
         D=0,
         J=0,
         kinetics=[
-            rp.kinetics.Exponential(k)
+            rp.kinetics.Haberkorn(k, State.SINGLET),
+            rp.kinetics.HaberkornFree(k),
         ],
+        relaxations=[relaxation.SingletTripletDephasing(k)],
     )
 
-    x = MARY["B"] * 1e3
+    x = MARY["B"]
 
     plt.plot(x, MARY["MARY"], color="red", linewidth=2)
     plt.xlabel("$B_0 (mT)$")
     plt.ylabel("MFE (%)")
-    plt.title(f"B={B}")
+    plt.title("")
     plt.legend([r"$P_i(t)$", r"$\Phi_i$"]); path = __file__[:-3] + f"_{0}.png"; plt.savefig(path)
     #plt.show()
 
