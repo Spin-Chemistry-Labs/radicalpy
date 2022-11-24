5a6050e56f0e89e72d83d8eebb34de1c62763a20
lmantill <72053628+lmantill@users.noreply.github.com>
2022-11-24 12:47:42 +0900

    Finish mary.py

--- examples/mary.py
+++ examples/mary.py
@@ -9,17 +9,17 @@ from radicalpy.simulation import State
 
 def main():
     flavin = rp.simulation.Molecule("flavin_anion", ["H25"])
-    #trp = rp.simulation.Molecule("tryptophan_cation", ["N1"])
-    trp = rp.simulation.Molecule("trp")
+    trp = rp.simulation.Molecule("tryptophan_cation", ["N1"])
+    #trp = rp.simulation.Molecule("trp")
     sim = rp.simulation.LiouvilleSimulation([flavin, trp])
     # sim = rp.simulation.HilbertSimulation([flavin, trp])
     time = np.arange(0, 5e-6, 5e-9)
-    Bs = np.arange(0, 10, 0.1)
-    k = 3e6
-    kSTD = 1e7
+    Bs = np.arange(0, 30, 0.1)
+    k = 10e7
+    kSTD = 3e8
 
     results = sim.MARY(
-        init_state=State.SINGLET,
+        init_state=State.TRIPLET,
         obs_state=State.TRIPLET,
         time=time,
         B=Bs,
@@ -36,7 +36,6 @@ def main():
     HFE = results["HFE"]
     LFE = results["LFE"]
 
-    print(results.keys())
     Bhalf, x_model_MARY, y_model_MARY, MARY_fit_error, R2 = rp.utils.Bhalf_fit(Bs, MARY)
 
     plt.plot(Bs, MARY, color="red", linewidth=2)
@@ -45,12 +44,12 @@ def main():
     plt.xlabel("$B_0 (mT)$")
     plt.ylabel("MFE (%)")
     plt.title("")
-    plt.legend([r"$P_i(t)$", r"$\Phi_i$"])
+    plt.legend([r"Simulation", r"Fit"])
     
     print(f"HFE = {HFE: .2f} %")
     print(f"LFE = {LFE: .2f} %")
     print(f"B1/2 = {Bhalf: .2f} mT")
-    print(f"B1/2 fit error = {MARY_fit_error: .2f} mT")
+    print(f"B1/2 fit error = {MARY_fit_error[1]: .2f} mT")
     print(f"R^2 for B1/2 fit = {R2: .3f}")
 
     path = __file__[:-3] + f"_{0}.png"
