221e9ce48c2a10cdd73cbc0e77f41e7ecb2e0fe8
lmantill <72053628+lmantill@users.noreply.github.com>
2022-11-27 20:12:20 +0900

    Finish `time_evolution.py`

--- examples/time_evolution.py
+++ examples/time_evolution.py
@@ -7,47 +7,36 @@ from radicalpy.simulation import State
 
 
 def main():
-    flavin = rp.simulation.Molecule("flavin_anion", ["H25"])
-    Z = rp.simulation.Molecule("Z")
-    sim = rp.simulation.HilbertSimulation([flavin, Z])
-    print(sim)
-    #sim = rp.simulation.LiouvilleSimulation([flavin, Z])
-    time = np.arange(0, 2e-6, 5e-9)
-    Bs = np.arange(0, 1, 1)
-    k = 3e6
-
-    results = sim.MARY(
-        init_state=State.SINGLET,
-        obs_state=State.TRIPLET,
-        time=time,
-        B=Bs,
-        D=0,
-        J=0,
-        kinetics=[rp.kinetics.Exponential(k)],
-        #kinetics=[
-         #   rp.kinetics.Haberkorn(k, State.SINGLET),
-          #  rp.kinetics.HaberkornFree(k),
-        #],
-    )
-
-    Bi = 0
-    B = Bs[Bi]
-    x = time * 1e6
-    PY = results["product_yield_sums"]
-    #print(results.keys())
-
-    plt.plot(x, results["time_evolutions"][Bi], color="red", linewidth=2)
-    plt.fill_between(x, results["product_yields"][Bi], color="blue", alpha=0.2)
-    plt.xlabel("Time ($\mu s$)")
-    plt.ylabel("Probability"); plt.ylim([0, 1])
-    #plt.title(f"B={B} mT")
-    plt.legend([r"$P_i(t)$", r"$\Phi_i$"])
-    path = __file__[:-3] + f"_{B}.png"
-    plt.savefig(path)
-    #plt.show()
+	flavin = rp.simulation.Molecule("flavin_anion", ["H25"])
+	print(flavin)
+	Z = rp.simulation.Molecule("Z")
+	print(Z)
+	sim = rp.simulation.HilbertSimulation([flavin, Z])
+	print(sim)
+	H = sim.total_hamiltonian(B=0, D=0, J=0)
 	
-    print(f"Product yield = {PY}")
+	time = np.arange(0, 2e-6, 5e-9)
+	rhos = sim.time_evolution(State.SINGLET, time, H)
+	
+	k = 3e6
+	kinetics=[rp.kinetics.Exponential(k)]
 
+	time_evol = sim.product_probability(State.TRIPLET, rhos)
+	sim.apply_hilbert_kinetics(time, time_evol, kinetics)
+	product_yields, product_yield_sums = sim.product_yield(time_evol, time, k)
+	
+	x = time * 1e6
+	
+	plt.plot(x, time_evol, color="red", linewidth=2)
+	plt.fill_between(x, product_yields, color="blue", alpha=0.2)
+	plt.xlabel("Time ($\mu s$)")
+	plt.ylabel("Probability"); plt.ylim([0, 1])
+	plt.legend([r"$P_i(t)$", r"$\Phi_i$"])
+	
+	print(f"PY = {product_yield_sums}")    
+	
+	path = __file__[:-3] + f"_{0}.png"
+	plt.savefig(path)
 
 if __name__ == "__main__":
     main()
