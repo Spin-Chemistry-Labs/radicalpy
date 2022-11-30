50c8de0e49ac4dfa49b4428a0e95c0e6761f7306
Emil VATAI <emil.vatai@gmail.com>
2022-11-30 23:08:58 +0900

    Add more plots (and examples)

--- examples/energy_level.py
+++ examples/energy_level.py
@@ -15,13 +15,8 @@ def main():
     eigval = np.linalg.eigh(H)
     E = np.real(eigval[0])  # 0 = eigenvalues, 1 = eigenvectors
 
-    fig = plt.figure(figsize=(4, 8))
-    ax = fig.add_axes([0, 0, 1, 1])
-    ax.eventplot(E, orientation="vertical", color="red", linewidth=3)
-    ax.set_ylabel("Spin state energy (J)", size=14)
-    plt.tick_params(labelsize=14)
-    path = __file__[:-3] + f"_{0}.png"
-    plt.savefig(path)
+    rp.plot.energy_levels(sim, B=np.arange(0, 1e3, 10), J=10, D=0)
+    plt.show()
 
 
 if __name__ == "__main__":
