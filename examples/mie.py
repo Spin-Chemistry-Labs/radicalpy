2c66309ac4f51f970515fc7e3d649bb7741f9448
lmantill <72053628+lmantill@users.noreply.github.com>
2022-12-11 22:13:22 +0900

    Create mie.py

---
+++ examples/mie.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import radicalpy as rp
+from radicalpy.simulation import State
+
+
+def main():
+    Py_h = rp.simulation.Molecule(nuclei=["1H"], hfcs=[0.073])
+    DMA_h = rp.simulation.Molecule(nuclei=["1H"], hfcs=[0.181])
+
+    Py_d = rp.simulation.Molecule(nuclei=["2H"], hfcs=[0.481])
+    DMA_d = rp.simulation.Molecule(nuclei=["2H"], hfcs=[1.18])
+
+    sim = rp.simulation.HilbertSimulation([Py_h, DMA_h])
+    sim2 = rp.simulation.HilbertSimulation([Py_d, DMA_d])
+
+    k = 3e6
+    time = np.arange(0, 5e-6, 5e-9)
+    Bs = np.arange(0, 30, 0.1)
+
+    results = sim.MARY(
+        init_state=State.SINGLET,
+        obs_state=State.SINGLET,
+        time=time,
+        B=Bs,
+        D=0,
+        J=0,
+        kinetics=[
+        rp.kinetics.Exponential(k),
+        ],
+    )
+    MARY = results["MARY"]
+    HFE = results["HFE"]
+    LFE = results["LFE"]
+
+    #Bhalf, x_model_MARY, y_model_MARY, MARY_fit_error, R2 = rp.utils.Bhalf_fit(Bs, MARY)
+
+    results2 = sim2.MARY(
+        init_state=State.SINGLET,
+        obs_state=State.SINGLET,
+        time=time,
+        B=Bs,
+        D=0,
+        J=0,
+        kinetics=[
+        rp.kinetics.Exponential(k),
+        ],
+    )
+    MARY2 = results2["MARY"]
+    HFE2 = results2["HFE"]
+    LFE2 = results2["LFE"]
+
+    #Bhalf2, x_model_MARY2, y_model_MARY2, MARY_fit_error2, R22 = rp.utils.Bhalf_fit(Bs, MARY2)
+
+    plt.plot(Bs, MARY, color="red", linewidth=2)
+    #plt.plot(x_model_MARY, y_model_MARY, "k--", linewidth=1, label="Lorentzian fit")
+    plt.plot(Bs, MARY2, color="blue", linewidth=2)
+    #plt.plot(x_model_MARY2, y_model_MARY2, "g--", linewidth=1, label="Lorentzian fit")
+
+    plt.xlabel("$B_0 (mT)$")
+    plt.ylabel("MFE (%)")
+    #plt.legend([r"Simulation", r"Fit"])
+
+    print(f"HFE = {HFE: .2f} %")
+    print(f"LFE = {LFE: .2f} %")
+    #print(f"B1/2 = {Bhalf: .2f} mT")
+    #print(f"B1/2 fit error = {MARY_fit_error[1]: .2f} mT")
+    #print(f"R^2 for B1/2 fit = {R2: .3f}")
+
+    print(f"HFE = {HFE2: .2f} %")
+    print(f"LFE = {LFE2: .2f} %")
+    #print(f"B1/2 = {Bhalf2: .2f} mT")
+    #print(f"B1/2 fit error = {MARY_fit_error2[1]: .2f} mT")
+    #print(f"R^2 for B1/2 fit = {R22: .3f}")
+
+    path = __file__[:-3] + f"_{0}.png"
+    plt.savefig(path)
+
+
+if __name__ == "__main__":
+    main()
