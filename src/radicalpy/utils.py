8a75d52b8016d2d2e32171121a1e11f8aeb4f7a6
lmantill <72053628+lmantill@users.noreply.github.com>
2022-12-14 15:29:34 +0900

    Update utils.py

--- src/radicalpy/utils.py
+++ src/radicalpy/utils.py
@@ -60,7 +60,8 @@ def autocorrelation(data: np.ndarray, factor=2) -> np.ndarray:
 
     Args:
             data (np.ndarray): The time dependent trajectory.
-                        factor (int): Data length reduction factor.
+
+			factor (int): Data length reduction factor.
 
     Returns:
             np.ndarray: The autocorrelation of the trajectory.
@@ -84,14 +85,19 @@ def Bhalf_fit(
 
     Args:
             B (np.ndarray): Magnetic field values (x-axis).
-                        MARY (np.ndarray): Magnetic field effect data (y-axis).
+
+			MARY (np.ndarray): Magnetic field effect data (y-axis).
 
     Returns:
             Bhalf (float): The magnetic field strength at half the saturation magnetic field.
-                        x_model_MARY (np.ndarray): x-axis from fit.
-                        y_model_MARY (np.ndarray): y-axis from fit.
-                        MARY_fit_error (float): Standard error for the fit.
-                        R2 (float): R-squared value for the fit.
+
+			x_model_MARY (np.ndarray): x-axis from fit.
+
+			y_model_MARY (np.ndarray): y-axis from fit.
+
+			MARY_fit_error (float): Standard error for the fit.
+
+			R2 (float): R-squared value for the fit.
 
     """
     popt_MARY, pcov_MARY = curve_fit(
@@ -121,13 +127,17 @@ def cartesian_to_spherical(
 
     Args:
             x (float or np.ndarray): Coordinate(s) in the x plane.
-                        y (float or np.ndarray): Coordinate(s) in the y plane.
-                        z (float or np.ndarray): Coordinate(s) in the z plane.
+
+            y (float or np.ndarray): Coordinate(s) in the y plane.
+
+            z (float or np.ndarray): Coordinate(s) in the z plane.
 
     Returns:
             r (float or np.ndarray): The radial distance(s).
-                        theta (float or np.ndarray): The polar angle(s).
-                        phi (float or np.ndarray): The azimuthal angle(s).
+
+			theta (float or np.ndarray): The polar angle(s).
+
+			phi (float or np.ndarray): The azimuthal angle(s).
 
     """
     r = np.sqrt(x**2 + y**2 + z**2)
@@ -199,7 +209,7 @@ def get_idx(values, target):
 
 
 def isotropic(anisotropic: np.ndarray or list) -> float:
-    """Convert tensors: Anisotropic tensor to isotropic value.
+    """Tensor transformation: Anisotropic tensor to isotropic value.
 
     Args:
             anisotropic (np.ndarray or list): The 3x3 interaction tensor matrix.
@@ -216,8 +226,10 @@ def Lorentzian_fit(x: np.ndarray, A: np.ndarray, Bhalf: float) -> np.ndarray:
 
     Args:
             x (np.ndarray): The x-axis values.
-                        A (np.ndarray): The amplitudes (intensity scaling).
-                        Bhalf (float): The magnetic field strength at half the saturation magnetic field.
+
+			A (np.ndarray): The amplitudes (intensity scaling).
+
+			Bhalf (float): The magnetic field strength at half the saturation magnetic field.
 
     Returns:
             np.ndarray: Lorentzian fit for MARY spectrum.
@@ -240,6 +252,15 @@ def MHz_to_angular_frequency(MHz: float) -> float:
 
 
 def MHz_to_Gauss(MHz: float) -> float:
+    """Convert units: Megahertz (MHz) to Gauss (G).
+
+    Args:
+            MHz (float): The frequency in Megahertz (MHz).
+
+    Returns:
+            float: Megahertz (MHz) converted to Gauss (G).
+
+    """
     g_e = constants.value("g_e")
     mu_B = constants.value("mu_B")
     h = constants.value("h")
@@ -247,6 +268,15 @@ def MHz_to_Gauss(MHz: float) -> float:
 
 
 def MHz_to_mT(MHz: float) -> float:
+    """Convert units: Megahertz (MHz) to milltesla (mT).
+
+    Args:
+            MHz (float): The frequency in Megahertz (MHz).
+
+    Returns:
+            float: Megahertz (MHz) converted to millitesla (mT).
+
+    """
     g_e = constants.value("g_e")
     mu_B = constants.value("mu_B")
     h = constants.value("h")
@@ -254,6 +284,15 @@ def MHz_to_mT(MHz: float) -> float:
 
 
 def mT_to_angular_frequency(mT: float) -> float:
+    """Convert units: millitesla (mT) to angular frequency (:math:`\\text{rad} \\cdot \\text{s}^{-1} \\cdot \\text{T}^{-1}`).
+
+    Args:
+            mT (float): The magnetic flux density in millitesla (mT).
+
+    Returns:
+            float: millitesla (mT) converted to angular frequency (:math:`\\text{rad} \\cdot \\text{s}^{-1} \\cdot \\text{T}^{-1}`).
+
+    """
     g_e = constants.value("g_e")
     mu_B = constants.value("mu_B")
     hbar = constants.value("hbar")
@@ -261,10 +300,28 @@ def mT_to_angular_frequency(mT: float) -> float:
 
 
 def mT_to_Gauss(mT: float) -> float:
+    """Convert units: millitesla (mT) to Gauss (G).
+
+    Args:
+            mT (float): The magnetic flux density in millitesla (mT).
+
+    Returns:
+            float: millitesla (mT) converted to Gauss (G).
+
+    """
     return mT * 10
 
 
 def mT_to_MHz(mT: float) -> float:
+    """Convert units: millitesla (mT) to Megahertz (MHz).
+
+    Args:
+            mT (float): The magnetic flux density in millitesla (mT).
+
+    Returns:
+            float: millitesla (mT) converted to Megahertz (MHz).
+
+    """
     g_e = constants.value("g_e")
     mu_B = constants.value("mu_B")
     h = constants.value("h")
@@ -272,50 +329,108 @@ def mT_to_MHz(mT: float) -> float:
 
 
 def multiexponential_fit(x, *args):
+    """Curve fitting: Multiexponential function for autocorrelation fitting.
+
+    Args:
+            x (np.ndarray): The x-axis values.
+
+            args (np.ndarray): The amplitudes (A) and taus for fitting.
+
+    Returns:
+            np.ndarray: Multiexponential fit for autocorrelations.
+
+    """
     n = len(args) // 2
     A, tau = list(args)[:n], list(args)[n:]
     return sum(a * np.exp(-t * x) for a, t in zip(A, tau))
 
 
-def rotation_matrix_x(phi: float) -> np.ndarray:
+def rotation_matrix_x(alpha: float) -> np.ndarray:
+    """Tensor transformation: Rotation matrix to rotate a vector or matrix by an angle alpha about the x-axis in 3D.
+
+    Args:
+            alpha (float): The angle to rotate a vector about the x-axis.
+
+    Returns:
+            np.ndarray: Rotated vector or matrix.
+
+    """
     return np.array(
         [
             [1, 0, 0],
-            [0, np.cos(phi), -np.sin(phi)],
-            [0, np.sin(phi), np.cos(phi)],
+            [0, np.cos(alpha), -np.sin(alpha)],
+            [0, np.sin(alpha), np.cos(alpha)],
         ]
     )
 
 
-def rotation_matrix_y(theta: float) -> np.ndarray:
+def rotation_matrix_y(beta: float) -> np.ndarray:
+    """Tensor transformation: Rotation matrix to rotate a vector or matrix by an angle beta about the y-axis in 3D.
+
+    Args:
+            beta (float): The angle to rotate a vector about the x-axis.
+
+    Returns:
+            np.ndarray: Rotated vector or matrix.
+
+    """
     return np.array(
         [
-            [np.cos(theta), 0, np.sin(theta)],
+            [np.cos(beta), 0, np.sin(beta)],
             [0, 1, 0],
-            [-np.sin(theta), 0, np.cos(theta)],
+            [-np.sin(beta), 0, np.cos(beta)],
         ]
     )
 
 
-def rotation_matrix_z(psi: float) -> np.ndarray:
+def rotation_matrix_z(gamma: float) -> np.ndarray:
+    """Tensor transformation: Rotation matrix to rotate a vector or matrix by an angle gamma about the z-axis in 3D.
+
+    Args:
+            gamma (float): The angle to rotate a vector about the x-axis.
+
+    Returns:
+            np.ndarray: Rotated vector or matrix.
+
+    """
     return np.array(
         [
-            [np.cos(psi), -np.sin(psi), 0],
-            [np.sin(psi), np.cos(psi), 0],
+            [np.cos(gamma), -np.sin(gamma), 0],
+            [np.sin(gamma), np.cos(gamma), 0],
             [0, 0, 1],
         ]
     )
 
 
-def spectral_density(omega, tau_c):
+def spectral_density(omega: float, tau_c: float) -> float:
+    """Spectral density: The frequency at which the motion of the particle exists.
+
+    Args:
+            omega (float): The Larmor frequency of the electron.
+
+			tau_c (float): The rotational correlation time.
+
+    Returns:
+            float: Spectral density frequency.
+
+    """
     return tau_c / (1 + omega**2 * tau_c**2)
 
 
-def spherical_average(singlet_yield, theta, phi):
-    # Subtracting the spherical average from the singlet yield
-    # Simpson's rule integration over theta (0, pi) and phi (0, 2pi)
-    # n_theta = odd, n_phi = even
+def spherical_average(product_yield: np.ndarray, theta: np.ndarray, phi: np.ndarray) -> float:
+    """Spherical average: The spherical average of anisotropic product yields.
 
+    Args:
+            product_yield (np.ndarray): The anisotropic product yields.
+
+			theta (np.ndarray): The angles theta by which the anisotropic product yields were calculated.
+
+			phi (np.ndarray): The angles phi by which the anisotropic product yields were calculated.
+
+    Returns:
+            float: The spherical average of the anisotropic product yields.
+
+    """
     theta, phi = _anisotropy_check(theta, phi)
     nth, nph = check_full_sphere_coordinates(theta, phi)
 
@@ -328,7 +443,7 @@ def spherical_average(singlet_yield, theta, phi):
     sintheta = np.sin(np.linspace(0, np.pi, nth))
 
     spherical_average = sum(
-        singlet_yield[i, j] * sintheta[i] * wt[i] * wp[j]
+        product_yield[i, j] * sintheta[i] * wt[i] * wp[j]
         for i in range(nth)
         for j in range(nph)
     )
@@ -336,7 +451,18 @@ def spherical_average(singlet_yield, theta, phi):
     return spherical_average * theta[1] * phi[1] / (4 * np.pi) / 9
 
 
-def spherical_to_cartesian(theta, phi):
+def spherical_to_cartesian(theta: float or np.ndarray, phi: float or np.ndarray) -> np.ndarray:
+    """Convert units: spherical coordinates to Cartesian coordinates.
+
+    Args:
+            theta (float or np.ndarray): The polar angle(s).
+
+			phi (float or np.ndarray): The azimuthal angle(s).
+
+    Returns:
+            np.ndarray: The Cartesian coordinates.
+
+    """
     return np.array(
         [
             np.sin(theta) * np.cos(phi),
@@ -347,6 +473,15 @@ def spherical_to_cartesian(theta, phi):
 
 
 def spin_quantum_number(multiplicity: int) -> float:
+    """Conversion: Multiplicity to spin quantum number.
+
+    Args:
+            multiplicity (int): Spin multiplicity.
+
+    Returns:
+            float: Spin quantum number.
+
+    """
     return float(multiplicity - 1) / 2.0
 
 
@@ -358,9 +493,24 @@ def square_vectors(rhos):
     return rhos
 
 
-def yield_anisotropy(yields, theta, phi):
-    delta_phi = yields.max() - yields.min()
-    yield_av = spherical_average(yields, theta, phi)
+def yield_anisotropy(product_yield: np.ndarray, theta: np.ndarray, phi: np.ndarray) -> (float, float):
+    """Calculate the yield anisotropy.
+
+    Args:
+            product_yield (np.ndarray): The anisotropic product yields.
+
+			theta (np.ndarray): The angles theta by which the anisotropic product yields were calculated.
+
+			phi (np.ndarray): The angles phi by which the anisotropic product yields were calculated.
+
+    Returns:
+            delta_phi (float): Maximum yield - minimum yield.
+
+			gamma (float): delta_phi / spherical average.
+
+    """
+    delta_phi = product_yield.max() - product_yield.min()
+    yield_av = spherical_average(product_yield, theta, phi)
     gamma = delta_phi / yield_av
     return delta_phi, gamma
 
