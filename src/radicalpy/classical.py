62bfe01760c3eb62f09107f6ed001dcb35b73602
lmantill <72053628+lmantill@users.noreply.github.com>
2022-12-18 21:12:25 +0900

    Add doc to `classical.py`

--- src/radicalpy/classical.py
+++ src/radicalpy/classical.py
@@ -6,19 +6,49 @@ import numpy as np
 from . import estimations, utils
 
 
-def get_delta_r(mutual_diffusion, delta_T):
+def get_delta_r(mutual_diffusion: float, delta_T: float) -> float:
+    """Mean path between two radicals.
+
+    Args:
+            mutual_diffusion (float): The mutual diffusion coefficient (m^2 s^-1).
+            delta_T (float): The time interval (s).
+
+    Returns:
+            float: The mean path between two radicals (m).
+    """
     return np.sqrt(6 * mutual_diffusion * delta_T)
 
 
 def _random_theta_phi():
+    """Random sampling of theta and phi.
+
+    Returns:
+            Theta and phi (radians).
+    """
     theta = np.pi * np.random.rand()
-    # phi = 2 * np.pi * np.random.rand()
     arg = np.random.uniform(-1, 1)
     phi = 2 * np.sign(arg) * np.arcsin(np.sqrt(np.abs(arg)))
     return theta, phi
 
 
-def randomwalk_3d(n_steps, x_0, y_0, z_0, delta_r, r_min, r_max=0):
+def randomwalk_3d(n_steps: float, x_0: float, y_0: float, z_0: float, delta_r: float, r_min: float, r_max: float = 0) -> (np.ndarray, np.ndarray, np.ndarray):
+    """Monte Carlo random walk for radicals pairs in both solution and microreactor environments.
+
+    Args:
+            n_steps (float): The number of simulation steps.
+            x_0 (float): The initial position in the x-axis (m).
+            y_0 (float): The initial position in the x-axis (m).
+            z_0 (float): The initial position in the x-axis (m).
+            delta_r (float): The mean path between two radicals (m).
+            r_min (float): The distance of closest approach (m).
+            r_max (float): The diameter of the microreactor (m). Set to 0 for solution-based simulations.
+
+    Returns:
+            (np.ndarray, np.ndarray, np.ndarray)
+            pos (np.ndarray): The positions of the moving radical (m).
+            dist (np.ndarray): The mutual distances between the radical pairs (m).
+            angle (np.ndarray): The angles (theta) of the vector trajectories of the moving radical (m).
+    """
     pos = np.zeros([n_steps, 3])
     dist = np.zeros(n_steps)
     angle = np.zeros(n_steps)
@@ -38,21 +68,3 @@ def randomwalk_3d(n_steps, x_0, y_0, z_0, delta_r, r_min, r_max=0):
         pos[i] = new_pos
         dist[i] = d
     return pos, dist, angle
-
-
-def monte_carlo_exchange_dipolar(n_steps, r_min, del_T, radA_x, dist, angle):
-
-    r_min = radA_x[0]
-    dist[0] = r_min
-    r = dist
-    r_tot = r_min + r
-
-    theta = angle
-
-    t_tot = n_steps * del_T * 1e9
-    t = np.linspace(0, t_tot, n_steps)
-
-    J = estimations.exchange_interaction_monte_carlo(r)
-    D = estimations.dipolar_interaction_monte_carlo(r_tot, theta)
-
-    return t, r_tot, J, D
