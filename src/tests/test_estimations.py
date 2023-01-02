589f02d1702c742e7b4d41a8569595a777640a41
lmantill <72053628+lmantill@users.noreply.github.com>
2023-01-02 22:51:09 +0900

    Update test_estimations.py

--- src/tests/test_estimations.py
+++ src/tests/test_estimations.py
@@ -110,4 +110,4 @@ class EstimationsTests(unittest.TestCase):
     def test_rotational_correlation_time_for_protein(self):
         gold = 3.595786684051539e-08
         tau_c = rp.estimations.rotational_correlation_time_for_protein(61, 278)
-        self.assertAlmostEqual(gold, viscosity)
+        self.assertAlmostEqual(gold, tau_c)
