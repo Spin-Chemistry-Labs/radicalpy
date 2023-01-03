83241e16dd4ae975581507865c5a30fed5d8f1bd
Emil VATAI <emil.vatai@gmail.com>
2023-01-03 09:34:23 +0900

    Add wip_experiments.py

---
+++ src/utils/wip_experiments.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+from pathlib import Path
+
+sys.path.insert(0, Path(__file__).parent)
+from types import SimpleNamespace
+
+from radicalpy import ureg
+from radicalpy.data import SPIN_DATA, gamma_mT
+
+
+class Constant(float):
+    def __new__(cls, some_arg=None):
+        obj = float.__new__(cls, 10)
+        obj._some_arg = some_arg
+        return obj
+
+
+const = Constant(some_arg="Hi")
+print(f"{const=}")
+print(f"{100.0 + const=}")
+print(f"{const._some_arg=}")
+
+
+class Isotope(str):
+    def __new__(cls, o):
+        obj = super().__new__(cls, o)
+        data = SPIN_DATA[o]
+        obj.gamma = data["gamma"]
+        obj.multiplicity = data["multiplicity"]
+        return obj
+
+
+print(SPIN_DATA["E"])
+print(gamma_mT("E"))
+e = Isotope("E")
+
+print(f"{e=}")
+print(f"{e.gamma=}")
+print(f"{e.multiplicity=}")
