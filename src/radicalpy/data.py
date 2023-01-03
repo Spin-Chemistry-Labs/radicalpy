035cee6fba697d2116175a4d6934dc065c4402a3
Emil VATAI <emil.vatai@gmail.com>
2023-01-03 20:03:56 +0900

    Implement `Isotope` class

--- src/radicalpy/data.py
+++ src/radicalpy/data.py
@@ -106,3 +106,26 @@ class Constant(float):
 
 
 constants = Constant.fromjson(CONSTANTS_JSON)
+
+
+class Isotope(str):
+    """Interface to isotope database."""
+
+    def __new__(cls, name, data):
+        # gamma_unit = ureg("rad/s/T")
+        obj = super().__new__(cls, name)
+        if "gamma" in data:
+            gamma = data.pop("gamma")
+            obj.gamma = gamma  # * gamma_unit
+            obj.gamma_mT = gamma / 1000
+        if "multiplicity" in data:
+            multiplicity = data.pop("multiplicity")
+            obj.multiplicity = multiplicity
+        obj.details = data
+        return obj
+
+    @staticmethod
+    def fromjson(json_file):
+        with open(json_file) as f:
+            data = json.load(f)
+        return SimpleNamespace(**{k: Isotope(k, v) for k, v in data.items()})
