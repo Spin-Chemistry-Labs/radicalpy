4448b2977fe877cb8b67879a0f093f666a2a3159
Emil VATAI <emil.vatai@gmail.com>
2023-01-06 15:30:45 +0900

    Add number of constants test

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -2,9 +2,13 @@
 
 import unittest
 
-from src.radicalpy.data import constants as C
+from ..radicalpy.data import constants as C
 
 
 class DataTests(unittest.TestCase):
+    def test_number_of_constants(self):
+        previous_number_of_tests = 21
+        self.assertEqual(len(vars(C)), previous_number_of_tests)
+
     def test_constants(self):
         self.assertEqual(C.mu_0, 1.25663706212e-06)
