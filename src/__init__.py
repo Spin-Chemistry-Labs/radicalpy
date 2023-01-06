2971f60465899f66a5e96b74d07a8fc9bd0e9616
Emil VATAI <emil.vatai@gmail.com>
2023-01-06 15:30:45 +0900

    Add number of constants test

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# The directory structure needs some rearrangement.  I'll do it after
+# issue-branch 62 is done.
+#
+# mv ./src/radicalpy .
+# mv ./src/tests .  # check if tests work like that???
+# mv ./src/utils ./radicalpy #                     ???
+# rm -rf ./src # including this file               !!!
