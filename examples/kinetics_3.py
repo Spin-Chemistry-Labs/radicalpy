546de860fa56ffc589ba3012b9f11caa02795fd2
lmantill <72053628+lmantill@users.noreply.github.com>
2023-01-09 12:08:16 +0900

    Clean up kinetics examples

--- examples/kinetics_3.py
+++ examples/kinetics_3.py
@@ -7,22 +7,6 @@ import radicalpy as rp
 
 
 def main():
-    def kinetics(time, initial_populations, states, rate_equations):
-        shape = (len(states), len(states))
-        arrange = [
-            rate_equations[i][j] if (i in rate_equations and j in rate_equations[i]) else 0
-            for i in states
-            for j in states
-        ]
-        rates = np.reshape(arrange, shape)
-        dt = time[1] - time[0]
-        result = np.zeros([len(time), *rates[0].shape], dtype=float)
-        propagator = sp.sparse.linalg.expm(sp.sparse.csc_matrix(rates) * dt)
-        result[0] = initial_populations
-        for t in range(1, len(time)):
-            result[t] = propagator @ result[t - 1]
-        return result
-
     # Kinetic simulation of intramolecular magnetic field effects.
     # The example given here is cryptochrome
 
@@ -124,8 +108,8 @@ def main():
 
     rates_off = {**base, **off}
     rates_on = {**base, **on}
-    result_off = kinetics(time, initial, my_states, rates_off)
-    result_on = kinetics(time, initial, my_states, rates_on)
+    result_off = rp.classical.kinetics(time, initial, my_states, rates_off)
+    result_on = rp.classical.kinetics(time, initial, my_states, rates_on)
 
     fluor_field_off = result_off[:, 0]
     fluor_field_on = result_on[:, 0]
