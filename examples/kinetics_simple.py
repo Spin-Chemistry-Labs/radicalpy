2b9e399b51103a0a00b4cb8bfaa6c259ab5efcd9
Emil VATAI <emil.vatai@gmail.com>
2023-03-22 20:17:05 +0900

    WIP latexify

--- examples/kinetics_simple.py
+++ examples/kinetics_simple.py
@@ -1,39 +1,100 @@
 #! /usr/bin/env python
+import os
+import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+print(f"cwd: os.getcwd()")
+sys.path.append(".")
 from radicalpy.classical import RateEquations
+
+
+class Rate:
+    """Rate class.
+
+    Extends float with the `Rate.label` which is the LaTeX
+    representation of the rate.
+
+    """
+
+    value: float
+    label: str
+    """LaTeX representation of the rate constant."""
+
+    def __repr__(self):
+        return f"{self.label} = {self.value}"
+
+    def __init__(self, value: float, label: str):  # noqa D102
+        self.value = value
+        self.label = label
+
+    def __rmul__(self, v):
+        return Rate(self.value * v, f"{v} {self.label}")
+
+    def __mul__(self, v):
+        return self.__rmul__(v)
+
+    @staticmethod
+    def _get_value_lable(v):
+        return (v.value, v.label) if isinstance(v, Rate) else (v, v)
+
+    def __radd__(self, v):
+        value, label = self._get_value_lable(v)
+        return Rate(self.value + value, f"{label} + {self.label}")
+
+    def __add__(self, v):
+        value, label = self._get_value_lable(v)
+        return Rate(self.value + value, f"{self.label} + {label}")
+
+    def __neg__(self):
+        return Rate(-self.value, f"-({self.label})")
+
+
+def latexify(rate_equations: dict):
+    print(rate_equations)
+    result = []
+    for k, v in rate_equations.items():
+        lhs = f"\\frac{{d[{k}]}}{{dt}} "
+        rhs = " + ".join([f"{edge.label} [{vertex}]" for vertex, edge in v.items()])
+        result.append(f"${lhs} = {rhs}$")
+    return result
 
 
 def main():
     # Simple example of a RP for the paper.
 
     # kinetic parameters
-    ke = 1e6  # geminate RP to free radical separation
-    kst = 8e7  # ST-mixing rate
-    krlx = 2e6  # RP relaxation rate
-    kr = 1e8  # spin selective reverse electron transfer of RP to groundstate
+    ke = Rate(1e6, "k_\\text{E}")  # geminate RP to free radical separation
+    kst = Rate(8e7, "k_\\text{ST}")  # ST-mixing rate
+    krlx = Rate(2e6, "k_\\text{Rlx}")  # RP relaxation rate
+    kr = Rate(1e8, "k_\\text{R}")  # reverse electron transfer of RP to groundstate
 
     # Rate equations
+    S, Tp, T0, Tm = "S", "T_+", "T_0", "T_-"
     off = {}
-    off["S"] = {"S": -(3 * kst + kr + ke), "T+": kst, "T0": kst, "T-": kst}
-    off["T+"] = {"T+": -(2 * kst + ke), "S": kst, "T0": kst}
-    off["T0"] = {"T0": -(3 * kst + ke), "S": kst, "T+": kst, "T-": kst}
-    off["T-"] = {"T-": -(2 * kst + ke), "S": kst, "T0": kst}
+    off[S] = {S: -(3 * kst + kr + ke), Tp: kst, T0: kst, Tm: kst}
+    off[Tp] = {Tp: -(2 * kst + ke), S: kst, T0: kst}
+    off[T0] = {T0: -(3 * kst + ke), S: kst, Tp: kst, Tm: kst}
+    off[Tm] = {Tm: -(2 * kst + ke), S: kst, T0: kst}
 
     on = {}
-    on["S"] = {"S": -(kst + 2 * krlx + kr + ke), "T+": krlx, "T0": kst, "T-": krlx}
-    on["T+"] = {"T+": -(2 * krlx + ke), "S": krlx, "T0": krlx}
-    on["T0"] = {"T0": -(kst + 2 * krlx + ke), "S": kst, "T+": krlx, "T-": krlx}
-    on["T-"] = {"T-": -(2 * krlx + ke), "S": krlx, "T0": krlx}
+    on[S] = {S: -(kst + 2 * krlx + kr + ke), Tp: krlx, T0: kst, Tm: krlx}
+    on[Tp] = {Tp: -(2 * krlx + ke), S: krlx, T0: krlx}
+    on[T0] = {T0: -(kst + 2 * krlx + ke), S: kst, Tp: krlx, Tm: krlx}
+    on[Tm] = {Tm: -(2 * krlx + ke), S: krlx, T0: krlx}
 
-    initial_states = {"T+": 1 / 3, "T0": 1 / 3, "T-": 1 / 3}
+    for eq in latexify(off):
+        print(eq)
+    exit()
+
+    initial_states = {Tp: 1 / 3, T0: 1 / 3, Tm: 1 / 3}
     time = np.linspace(0, 1e-6, 10000)
 
     result_off = RateEquations(off, time, initial_states)
     result_on = RateEquations(on, time, initial_states)
 
-    keys = ["S", "T+", "T0", "T-"]
+    keys = [S, Tp, T0, Tm]
     rp_field_off = result_off[keys]
     rp_field_on = result_on[keys]
     rp_delta_delta_A = rp_field_on - rp_field_off
@@ -57,6 +118,10 @@ def main():
     fig.set_size_inches(10, 5)
     path = __file__[:-3] + f"_{0}.png"
     plt.savefig(path)
+
+    latex = latexify(off)
+    print("--------- space -----------")
+    print(latex)
 
 
 if __name__ == "__main__":
