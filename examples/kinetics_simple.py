461a95fc48cf4685616688d53c90ed8da3e6032a
Emil VATAI <emil.vatai@gmail.com>
2023-03-22 23:18:25 +0900

    WIP

--- examples/kinetics_simple.py
+++ examples/kinetics_simple.py
@@ -1,18 +1,58 @@
 #! /usr/bin/env python
 
 import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 from radicalpy.classical import Rate, RateEquations, latex_eqlist_to_align, latexify
+
+
+def graph(rate_equations: dict):
+    data = [
+        (f"${v1}$", f"${v2}$", f"${edge.label}$")
+        for v1, rhs_data in rate_equations.items()
+        for v2, edge in rhs_data.items()
+    ]
+    for t in [e for _, _, e in data]:
+        print(t)
+    G = nx.DiGraph([(v1, v2) for v1, v2, e in data])
+    pos = nx.spring_layout(G, seed=42)
+    options = {
+        "font_size": 36,
+        "node_size": 5000,
+        # "node_color": "white",
+        "edgecolors": "black",
+        "linewidths": 2,
+        "width": 3,
+    }
+
+    plt.clf()
+    nx.draw_networkx(G, pos, with_labels=True, **options)
+    # nx.draw_networkx_edges(
+    #     G,
+    #     pos,
+    #     connectionstyle="arc3, rad=0.1",
+    # )
+    nx.draw_networkx_edge_labels(
+        G,
+        pos,
+        edge_labels={(v1, v2): e for v1, v2, e in data},
+        verticalalignment="top",
+    )
+    ax = plt.gca()
+    ax.margins(0.20)
+    plt.axis("off")
+    path = f"{__file__[:-3]}_graph.png"
+    plt.savefig(path)
 
 
 def main():
     # Simple example of a RP for the paper.
 
     # kinetic parameters
-    ke = Rate(1e6, "k_\\text{E}")  # geminate RP to free radical separation
-    kst = Rate(8e7, "k_\\text{ST}")  # ST-mixing rate
-    krlx = Rate(2e6, "k_\\text{Rlx}")  # RP relaxation rate
-    kr = Rate(1e8, "k_\\text{R}")  # reverse electron transfer of RP to groundstate
+    ke = Rate(1e6, "k_{E}")  # geminate RP to free radical separation
+    kst = Rate(8e7, "k_{ST}")  # ST-mixing rate
+    krlx = Rate(2e6, "k_{Rlx}")  # RP relaxation rate
+    kr = Rate(1e8, "k_{R}")  # reverse electron transfer of RP to groundstate
 
     # Rate equations
     S, Tp, T0, Tm = "S", "T_+", "T_0", "T_-"
@@ -60,6 +100,7 @@ def main():
     plt.savefig(path)
 
     print(latex_eqlist_to_align(latexify(off)))
+    graph(off)
 
 
 if __name__ == "__main__":
