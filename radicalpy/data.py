4a83a7d201ba0d98dbcabfa38af0a25aa61472cc
Emil VATAI <emil.vatai@gmail.com>
2023-05-18 21:28:31 +0900

    Fix underscore

--- radicalpy/data.py
+++ radicalpy/data.py
@@ -4,8 +4,8 @@ from functools import singledispatchmethod
 from typing import Optional
 
 import numpy as np
-from importlib-resources import files
-from importlib-resources.abc import Traversable
+from importlib_resources import files
+from importlib_resources.abc import Traversable
 from numpy.typing import NDArray
 
 
