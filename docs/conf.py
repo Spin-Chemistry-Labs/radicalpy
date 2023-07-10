125b475d8442b1e41c4ddb5ffba5749d627c6cee
Emil VATAI <emil.vatai@gmail.com>
2023-07-10 17:02:42 +0900

    New docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
+import re
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
@@ -12,29 +13,30 @@ sys.path.insert(0, os.path.abspath("../radicalpy"))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "RadicalPy"
-copyright = "2022, Lewis M. Antill"
-author = "Lewis M. Antill"
-release = "0.1rc4"
+copyright = "2022, Lewis M. Antill and Emil Vatai"
+author = "Lewis M. Antill and Emil Vatai"
+release = re.sub("^v", "", os.popen("git describe --tags").read().strip())
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.todo",
     "sphinx.ext.coverage",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    # "sphinx_autodoc_typehints", # did't work when I tried :(
     "sphinx_rtd_theme",
 ]
 
+default_role = "py:obj"
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # autoclass_content = "both"
 todo_include_todos = True
-default_role = "py:obj"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
