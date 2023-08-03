ac04fc944f494eda3249d1de78e86a8976010844
Emil VATAI <emil.vatai@gmail.com>
2023-08-03 18:56:03 +0900

    Fix example

--- examples/kinetics_simple.py
+++ examples/kinetics_simple.py
@@ -6,7 +6,8 @@ import dot2tex
 import graphviz
 import matplotlib.pyplot as plt
 import numpy as np
-from radicalpy.classical import Rate, RateEquations, latex_eqlist_to_align, latexify
+from radicalpy.classical import (Rate, RateEquations, latex_eqlist_to_align,
+                                 latexify, reaction_scheme)
 
 
 def main():
@@ -70,7 +71,7 @@ def main():
     # for eq in latexify(off):
     #     print(eq)
     # print(latex_eqlist_to_align(latexify(off)))
-    reaction_scheme(on)
+    reaction_scheme(__file__, on)
 
 
 if __name__ == "__main__":
