01bbbe9a448820992c9195bc9857ae46bdce63b4
Emil VATAI <emil.vatai@gmail.com>
2023-12-07 19:13:50 +0900

    WIP

--- examples/sctep.py
+++ examples/sctep.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from radicalpy.data import Isotope, Molecule, Nucleus
+from radicalpy.data import Isotope, Molecule, Nucleus, Triplet
 from radicalpy.experiments import steady_state_mary
 from radicalpy.simulation import Basis, LiouvilleSimulation
 
@@ -9,28 +9,24 @@ def main():
     gamma = Isotope("E").gamma_mT
     D = -6.2 * gamma
     E = 35 * gamma
-    triplet = Nucleus(
-        magnetogyric_ratio=gamma,
-        multiplicity=3,
-        hfc=0.0,
-        name="Triplet",
-    )
-    m = Molecule(radical=triplet)
-    sim = LiouvilleSimulation(molecules=[m, m], basis=Basis.ZEEMAN)
-    # print(sim)
-    steady_state_mary(
-        sim,
-        obs_state=State.TP_SINGLET,
-        B=Bs,
-        D=D,
-        E=E,
-        J=0,
-        kinetics=[
-            rp.kinetics.Haberkorn(krec, State.TP_SINGLET),
-            rp.kinetics.HaberkornFree(kesc),
-        ],
-    )
-    print(H)
+    m = Triplet()
+    sim = LiouvilleSimulation(molecules=[m, m])
+    print(sim)
+    # sim = LiouvilleSimulation(molecules=[m, m], basis=Basis.ZEEMAN)
+    # # print(sim)
+    # steady_state_mary(
+    #     sim,
+    #     obs_state=State.TP_SINGLET,
+    #     B=Bs,
+    #     D=D,
+    #     E=E,
+    #     J=0,
+    #     kinetics=[
+    #         rp.kinetics.Haberkorn(krec, State.TP_SINGLET),
+    #         rp.kinetics.HaberkornFree(kesc),
+    #     ],
+    # )
+    # print(H)
 
 
 if __name__ == "__main__":
