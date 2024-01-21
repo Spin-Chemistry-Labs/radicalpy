ff325fb99b137b42f656493b3c7f657ebacb2d57
Emil VATAI <emil.vatai@gmail.com>
2024-01-21 17:16:17 +0900

    SCTEP looks good

--- examples/sctep.py
+++ examples/sctep.py
@@ -1,21 +1,22 @@
 #! /usr/bin/env python
 
 import numpy as np
+
 from radicalpy.data import Isotope, Molecule, Nucleus, Triplet
 from radicalpy.experiments import steady_state_mary
 from radicalpy.kinetics import Haberkorn, HaberkornFree
 from radicalpy.simulation import Basis, HilbertSimulation, State
 
 
-def main():
-    # gamma = Isotope("E").gamma_mT
-    Bs = np.arange(0, 2500, 1)
-    D = -6.2  # * gamma
-    E = 35  #  * gamma
-    J = 499.55
-    k0 = 1
-    ks = 1.1e9
-    kd = 2.8e9
+def main(
+    Bs=np.arange(0, 2500, 1),
+    D=-6.2,
+    E=35,
+    J=499.55,
+    k0=1,
+    ks=1.1e9,
+    kd=2.8e9,
+):
     m = Triplet()
     sim = HilbertSimulation(molecules=[m, m], basis=Basis.ZEEMAN)
     print(sim)
