c6a7cc3a857236256d30d00c33e5c54c9636a9c3
Emil VATAI <emil.vatai@gmail.com>
2024-01-30 13:13:06 +0900

    Try some things

--- radicalpy/__init__.py
+++ radicalpy/__init__.py
@@ -1,20 +1,16 @@
 """Radicalpy package root."""
+import logging
+
 from pint import UnitRegistry
 
-from . import (  # noqa: F401 F403
-    classical,
-    data,
-    estimations,
-    kinetics,
-    plot,
-    relaxation,
-    shared,
-    simulation,
-    utils,
-)
+logger = logging.getLogger(__name__)
+print(f"{logger.name=}")
 
 # pylint: disable=unused-import wildcard-import
-
+# from . import (classical, data, estimations, kinetics, plot,  # noqa: E402 F401 F403
+# relaxation, shared, simulation, utils)
 
 ureg = UnitRegistry()
 Q_ = ureg.Quantity
+
+print(f"radicalpy.__init__ : {__name__}")
