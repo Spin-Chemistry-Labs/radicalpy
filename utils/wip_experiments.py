ff142f3233fbdd91bed4382ed794ad34050e65c2
Emil VATAI <emil.vatai@gmail.com>
2024-03-18 20:09:43 +0900

    Update WIP file

--- utils/wip_experiments.py
+++ utils/wip_experiments.py
@@ -9,68 +9,59 @@ from functools import singledispatchmethod  # noqa E402
 from typing import Optional
 
 import numpy as np
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike, NDArray
+
 from radicalpy import data  # noqa E402
 
 
-class Hfc:
-    """The Hfc class represents isotropic and anisotropic HFC values.
+def main():
+    rng = np.random.default_rng()
+    num_samples = 4000000
+    num_molecules = 2
+    num_dims = 3
+    samples = np.random.multivariate_normal(
+        mean=[0, 0],
+        cov=np.diag([1, 2]),
+        size=(num_dims, num_samples),
+    )
+    # axis, num_samples, num_molecules
+    samples_molecule0 = samples[:, :, 0]
+    samples_molecule1 = samples[:, :, 1]
+    norm = np.linalg.norm(samples_molecule0, axis=0)
+    print(f"{np.std(norm)=}")
+    print(f"{np.var(norm)=}")
 
-    Args:
-        hfc (float | list[list[float]]): The HFC value.  In case of a
-            single `float`, only the isotropic value is set.  In case
-            of a 3x3 matrix both the isotropic and anisotropic values
-            are stored.
-    """
-
-    _anisotropic: Optional[NDArray]
-    """Optional anisotropic HFC value."""
-
-    isotropic: float
-    """Isotropic HFC value."""
-
-    def __repr__(self):  # noqa D105
-        available = "not " if self._anisotropic is None else ""
-        return f"{self.isotropic:.4} <anisotropic {available}available>"
-
-    @singledispatchmethod
-    def __init__(self, hfc: list[list[float]]):
-        """Construct anisotropic `Hfc`."""
-        self._anisotropic = np.array(hfc)
-        if self._anisotropic.shape != (3, 3):
-            raise ValueError("Anisotropic HFCs should be a 3x3 matrix or a float!")
-        self.isotropic = self._anisotropic.trace() / 3
-
-    @__init__.register
-    def _(self, hfc: float):
-        """Construct isotropic only `Hfc`."""
-        self._anisotropic = None
-        self.isotropic = hfc
-
-    @property
-    def anisotropic(self) -> NDArray:
-        """Anisotropic value if available.
-
-        Returns:
-            NDarray: The anisotropic HFC values.
-        """
-        if self._anisotropic is None:
-            raise ValueError("The molecule doesn't support anisotropic HFCs")
-        return self._anisotropic
+    p = np.array(
+        [
+            np.diag([1, 1, 1]),
+            np.diag([2, 2, 2]),
+            np.diag([3, 3, 3]),
+        ]
+    )
+    print(f"{p.shape=}")
+    print(f"{samples.shape=}")
+    samples[:, 0, 0] = [100, 10, 1]
+    prod = np.einsum("anm,axy->nmxy", samples, p)
+    print(f"{prod.shape=}")
+    print(prod[0, 0, :, :])
 
 
-# with(open(DATA_DIR/"molecules/flavin_anion.json") as f:
-#      flavin_dict = json.load(f)
-flavin_dict = data.MOLECULE_DATA["flavin_anion"]
-hfc_3d_data = flavin_dict["data"]["N5"]["hfc"]
-hfc_3d_obj = Hfc(hfc_3d_data)
-print(f"{hfc_3d_obj=}")
-print(f"{hfc_3d_obj.isotropic=}")
-print(f"{hfc_3d_obj.anisotropic=}")
+def dist():
+    rng = np.random.default_rng()
+    num_samples = 40000
+    num_dims = 3
+    samples = np.random.multivariate_normal(
+        mean=[0, 0, 0],
+        cov=np.diag([1, 2, 3]),
+        size=(num_samples),
+    )
+    print(f"{samples.shape=}")
+    norm = np.linalg.norm(samples, axis=1)
+    print(f"{norm.shape=}")
+    print(f"{np.std(norm)**2=}")
+    print(f"{np.var(norm)**2=}")
 
-adenine_dict = data.MOLECULE_DATA["adenine_cation"]
-hfc_1d_data = adenine_dict["data"]["N6-H1"]["hfc"]
-hfc_1d_obj = Hfc(hfc_1d_data)
-print(f"{hfc_1d_obj=}")
-print(f"{hfc_1d_obj.isotropic=}")
-print(f"{hfc_1d_obj.anisotropic=}")
+
+if __name__ == "__main__" or True:
+    # main()
+    dist()
