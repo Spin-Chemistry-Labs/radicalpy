58b3569592dacb2b1a8d2f655f1c2a566f9fe42b
Emil VATAI <emil.vatai@gmail.com>
2024-03-19 09:33:42 +0900

    Blacken

--- examples/kinetics_simple.py
+++ examples/kinetics_simple.py
@@ -6,8 +6,9 @@ import dot2tex
 import graphviz
 import matplotlib.pyplot as plt
 import numpy as np
-from radicalpy.classical import (Rate, RateEquations, latex_eqlist_to_align,
-                                 latexify, reaction_scheme)
+
+from radicalpy.classical import (Rate, RateEquations, latex_eqlist_to_align, latexify,
+                                 reaction_scheme)
 
 
 def main():
