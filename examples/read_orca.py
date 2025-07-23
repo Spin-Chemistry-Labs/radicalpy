3290ffdfaed7d497f27a06cd9b279a7dcc7fae05
Kentaro Hino <chem2016@OX.AC.UK>
2025-07-23 18:16:43 +0100

    lint to pass the github actions

--- examples/read_orca.py
+++ examples/read_orca.py
@@ -11,7 +11,9 @@ print(molecule)
 
 
 print("\nRead from .property.txt file")
-indices, isotopes, hfc_matrices = rp.utils.read_orca("./examples/data/NH2_A.property.txt")
+indices, isotopes, hfc_matrices = rp.utils.read_orca(
+    "./examples/data/NH2_A.property.txt"
+)
 nuclei = [
     rp.data.Nucleus.fromisotope(isotope, hfc_matrix.tolist())
     for isotope, hfc_matrix in zip(isotopes, hfc_matrices)
