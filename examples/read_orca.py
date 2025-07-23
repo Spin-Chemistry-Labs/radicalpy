2a2ca9ce5368d3dc8eed5db3d9a779acf1bc8113
Kentaro Hino <chem2016@OX.AC.UK>
2025-07-23 18:09:09 +0100

    implemented read_orca for version 6

---
+++ examples/read_orca.py
@@ -0,0 +1,20 @@
+import radicalpy as rp
+
+print("Read from .out file")
+indices, isotopes, hfc_matrices = rp.utils.read_orca("data/NH2_A.out")
+nuclei = [
+    rp.data.Nucleus.fromisotope(isotope, hfc_matrix.tolist())
+    for isotope, hfc_matrix in zip(isotopes, hfc_matrices)
+]
+molecule = rp.simulation.Molecule("mol1", nuclei)
+print(molecule)
+
+
+print("\nRead from .property.txt file")
+indices, isotopes, hfc_matrices = rp.utils.read_orca("data/NH2_A.property.txt")
+nuclei = [
+    rp.data.Nucleus.fromisotope(isotope, hfc_matrix.tolist())
+    for isotope, hfc_matrix in zip(isotopes, hfc_matrices)
+]
+molecule = rp.simulation.Molecule("mol1", nuclei)
+print(molecule)
