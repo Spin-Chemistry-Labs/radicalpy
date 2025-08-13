e571d679497e040860065c56c8fd090a3f6ce838
Kentaro Hino <chem2016@OX.AC.UK>
2025-08-13 10:27:14 +0100

    amend examples/read_orca.py

--- examples/read_orca.py
+++ examples/read_orca.py
@@ -1,7 +1,9 @@
 import radicalpy as rp
 
 print("Read from .out file")
-indices, isotopes, hfc_matrices = rp.utils.read_orca_hyperfine("./examples/data/NH2_A.out")
+indices, isotopes, hfc_matrices = rp.utils.read_orca_hyperfine(
+    "./examples/data/NH2_A.out"
+)
 nuclei = [
     rp.data.Nucleus.fromisotope(isotope, hfc_matrix.tolist())
     for isotope, hfc_matrix in zip(isotopes, hfc_matrices)
