66bd9df847bb4827795fbedb1e4212480c41b17b
lmantill <72053628+lmantill@users.noreply.github.com>
2025-09-17 23:01:10 +0900

    Finish `data.py` and `experiments.py`

--- radicalpy/data.py
+++ radicalpy/data.py
@@ -377,7 +377,7 @@ class FuseNucleus(Nucleus):
     into a single effective nucleus for computational efficiency in spin
     dynamics calculations.
 
-    See also: `Phys. Rev. Lett. 120, 220604 (2019)`_.
+    See also: `Lindoy et al. Phys. Rev. Lett. 120, 220604 (2019)`_.
 
     Warning:
         This class should only be instantiated via the `from_nuclei` class method.
@@ -414,7 +414,7 @@ class FuseNucleus(Nucleus):
                [ 0. ,  0. ,  0. ,  0. ,  0.5,  0. ],
                [ 0. ,  0. ,  0. ,  0. ,  0. , -0.5]])
 
-    .. _Phys. Rev. Lett. 120, 220604 (2019):
+    .. _Lindoy et al. Phys. Rev. Lett. 120, 220604 (2019):
        https://doi.org/10.1103/PhysRevLett.120.220604
     """
 
